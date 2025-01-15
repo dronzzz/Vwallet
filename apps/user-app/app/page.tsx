4f7ed019276f9583693d09703cab7937c86e1f31
dronzzz <mayank25rathod@gmail.com>
2025-01-15 23:57:28 +0530

    fixed workflow error

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,18 +1,9 @@
-"use client"
-
-
-
-
+"use client";
 
 export default function Home() {
-
- 
   return (
     <>
-    
-   <div className="text-green-500">
-    new user interface
-   </div>
+      <div className="text-green-500">new user interface</div>
     </>
   );
 }
